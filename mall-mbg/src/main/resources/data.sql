e2c45d293ad1add725f44ba4b661beccb7cee903
zhaoliangliang <281365128@qq.com>
2023-03-05 21:46:17 +0800

    测试 mybatis generator

--- mall-mbg/src/main/resources/data.sql
+++ mall-mbg/src/main/resources/data.sql
@@ -1,2 +1,13 @@
 DROP DATABASE IF EXISTS mall;
 CREATE DATABASE mall;
+
+CREATE TABLE mall.user
+(
+    id               INT(11) NOT NULL AUTO_INCREMENT,
+    user_id          INT(11)        DEFAULT NULL,
+    product_id       INT(11)        DEFAULT NULL,
+    pay_amount       DECIMAL(10, 0) DEFAULT NULL,
+    add_time         DATETIME       DEFAULT CURRENT_TIMESTAMP,
+    last_update_time DATETIME       DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    PRIMARY KEY (id)
+) ENGINE = InnoDB AUTO_INCREMENT = 1 DEFAULT CHARSET = utf8
